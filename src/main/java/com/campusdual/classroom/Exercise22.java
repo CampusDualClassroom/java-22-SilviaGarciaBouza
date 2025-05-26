23e5acdb889d17cf2bc8fe3aa362274800ec189e
SilviaGarciaBouza <silvia.gar.bou.dev@gmail.com>
2025-05-26 12:07:34 +0200

    Ejercicio 22 completado

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -7,19 +7,34 @@ import java.util.Set;
 public class Exercise22 {
 
     public static List<String> createArrayList() {
+        List<String> myList = new ArrayList<>();
+        myList.add("GKFFD");
+        myList.add("TNANA");
+        myList.add("MPMSL");
+        myList.add("PSWME");
+        myList.add("LZMLF");
+        myList.add("JYEBV");
+        myList.add("YELNT");
+        myList.add("JSNKR");
+        myList.add("JFESF");
+        myList.add("TMJLL");
 
+        return myList;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+            for(int i=0; i<customList.size();i++){
+                System.out.println(customList.get(i));
+                System.out.println(i);
+            }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        return customList.add(element);
     }
 
     public static void main(String[] args) {
-
+        printElementsAndIndex(createArrayList());
     }
 
 
